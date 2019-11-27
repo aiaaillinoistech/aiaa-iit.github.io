c7e9f639caeb11621d13443e58a9980bc5f9cfc1
Mikel Woo <aiaa@iit.edu>
2019-11-26 22:56:44 -0600

    Cleaned up file structure and automated repeated page building

---
+++ js/main.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+    var head = document.createElement("div");
+    head.setAttribute("class", "header");
+    var title = document.createElement("h1");
+    title.appendChild(this.document.createTextNode("AIAA @ Illinois Tech"));
+    head.appendChild(title);
+    document.body.appendChild(head);
+    
+    var menu = document.createElement("div");
+    menu.setAttribute("class", "topnav");
+    document.body.appendChild(menu);
+
+    var menuitems = [["Home", "index.html"],
+        ["About", "about.html"],
+        ["DBF", "dbf.html"],
+        ["Rocketry", "rocketry.html"],
+        ["Contact Us", "contact.html"]];
+
+    var parts = window.location.pathname.split('/');
+    var page = "";
+    if (parts.length == 1) {
+        page = "index.html";
+    } else {
+        page = parts.pop().split("#")[0];
+    }
+
+    for (var i = 0; i < menuitems.length; i++) {
+        var link = document.createElement("a");
+        link.setAttribute("href", menuitems[i][1]);
+        link.appendChild(document.createTextNode(menuitems[i][0]));
+
+        if (menuitems[i][1] == page) {
+            link.setAttribute("class", "active");
+        }
+
+        menu.appendChild(link);
+    }
+
+    var foot = document.createElement("div");
+    foot.setAttribute("class", "footer");
+    var fcon = document.createElement("p");
+    fcon.appendChild(document.createTextNode("AIAA IIT"));
+    foot.appendChild(fcon);
+    document.body.appendChild(foot);
+};
