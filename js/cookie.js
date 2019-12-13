08ee34c7144010baa2ce405bb263f630f78f4142
mikwoo <mwoo@cnm.edu>
2019-12-12 19:19:51 -0700

    Using cookies to save some loading time for news

---
+++ js/cookie.js
@@ -0,0 +1,14 @@
+function getCookie(name) {
+    name += "=";
+    var ca = decodeURIComponent(document.cookie).split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
