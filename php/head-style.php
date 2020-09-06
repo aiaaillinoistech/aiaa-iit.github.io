8ec9796bbede34e16d0ba1cc6f0edca10eae9cd8
mwoo1 <mwoo1@hawk.iit.edu>
2020-09-05 23:52:10 -0600

    - Changed how titles are written. - Mobile topnav no longer hidden by things

--- php/head-style.php
+++ php/head-style.php
@@ -1,3 +1,8 @@
+<title>
+    <?php
+        echo $page . ' | AIAA IIT'
+    ?>
+</title>
 <link rel="icon" href="img/icon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="stylesheet" type="text/css" href="css/style.css">
